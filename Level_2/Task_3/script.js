ef466b4ae8783302360be46e60d1269a738a75af
Shivesh Mukund Tripathi <143014986+shiveshmukund@users.noreply.github.com>
2024-04-09 12:06:16 +0530

    script.js

--- Level_2/Task_3/script.js
+++ Level_2/Task_3/script.js
@@ -1,11 +1,8 @@
-document.querySelector('#push').onclick = function()
-{
-    if(document.querySelector('#newtask input').value.length == 0)
-    {
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task")
     }
-    else
-    {
+    else{
         document.querySelector('#tasks').innerHTML += `
             <div class="task">
                 <span id="taskname">
@@ -16,24 +13,18 @@ document.querySelector('#push').onclick = function()
                 </button>
             </div>
         `;
-
         var current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function()
-            {
+            current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
         }
-
         var tasks = document.querySelectorAll(".task");
-        for(var i=0; i<tasks.length; i++)
-        {
-            tasks[i].onclick = function()
-            {
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
                 this.classList.toggle('completed');
             }
         }
-
         document.querySelector("#newtask input").value = "";
     }
 }
